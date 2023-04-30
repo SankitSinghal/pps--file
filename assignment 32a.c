b4d68814672dabcdec728a0ab92627f18193824d
SankitSinghal <131650367+SankitSinghal@users.noreply.github.com>
2023-04-30 19:44:46 +0530

    Add files via upload

---
+++ assignment 32a.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    int rows, i, j, k;
+    
+    printf("Enter the number of rows: ");
+    scanf("%d", &rows);
+
+    printf("\nStar Pattern 1:\n");
+    for(i=1; i<=rows; i++) {
+        for(j=1; j<=i; j++) {
+            printf("*");
+        }
+        printf("\n");
+    }
